5da58b41849ce1314b66da59b3473ea7adb73672
KMTaylor023 <kmtaylor023@gmail.com>
2018-04-27 15:46:09 -0400

    working on game states

--- constants.py
+++ constants.py
@@ -7,8 +7,25 @@ B1_Y = .9
 B2_X = .45
 B2_Y = .9
 
-B3_X = .6
-B3_Y = .9
+
+#menu buttons: start, main, restart
+B4_X = .6
+B4_Y = .9
+
+B5_X = .6
+B5_Y = .9
+
+B6_X = .6
+B6_Y = .9
+
+#Text Positions
+MAIN_Y = .1
+OVER_Y = .1
+
+#Game states
+MAIN = 0
+GAME = 1
+OVER = 2
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
--- game.py
+++ game.py
@@ -23,14 +23,26 @@ class TuxExplorer():
             # We wont need this when we are not outputting the answer. 
             screen.fill(BLACK);
             text = font.render(answer, True, WHITE, BLACK)
-            screen.blit(text, ((x / 2) - (x / 10), (y / 2) - (y / 10)))
+            screen.blit(text, (half_x - (x / 10), half_y - (y / 10)))
+			
+			
+		def startGame():
+			state = GAME
          
+		def loseGame():
+			state = OVER
+		
+		def mainMenu():
+			statte = MAIN
         pygame.init()
         
         global x, y, font, text, screen
 
         x = gtk.gdk.screen_width()
         y = gtk.gdk.screen_height() - 55
+		
+		half_x = x / 2
+		half_y = y / 2
 
         pygame.display.set_caption('Angles')
 
@@ -46,6 +58,23 @@ class TuxExplorer():
         rightBut = Button("Right", x*B1_X, y*B1_Y, x*BUT_W, y*BUT_H, screen, saveAnswer)
         acuteBut = Button("Acute", x*B2_X, y*B2_Y, x*BUT_W, y*BUT_H, screen, saveAnswer)
         obtBut = Button("Obtuse", x*B3_X, y*B3_Y, x*BUT_W, y*BUT_H, screen, saveAnswer)
+		
+		#Game State Stuff
+		startBut = Button("Start", x*B4_X, y*B4_Y, x*BUT_W, y*BUT_H, screen, startGame)
+		mainBut = Button("Main Menu", x*B5_X, y*B5_Y, x*BUT_W, y*BUT_H, screen, mainMenu)
+		restartBut = Button("Restart", x*B6_X, y*B6_Y, x*BUT_W, y*BUT_H, screen, startGame)
+		
+		mainText = font.render("Tux Explorer", True, WHITE, BLACK)
+		mainHalfWidth = mainText.get_width() / 2
+		main_xpos = half_x - mainHalfWidth
+		main_ypos = MAIN_Y * y
+		
+		overText = font.render("Game Over!", True, WHITE, BLACK)
+		overHalfWidth = overText.get_width() / 2
+		over_xpos = half_x - overHalfWidth
+		over_ypos = OVER_Y * y
+		
+		state = MAIN
         
         while 1:
             while gtk.events_pending():
@@ -57,12 +86,24 @@ class TuxExplorer():
                 elif event.type == pygame.VIDEORESIZE:
                     pygame.display.set_mode(event.size, pygame.RESIZABLE)
 
-            acuteBut.draw()
-            rightBut.draw()
-            obtBut.draw()
-              
-            angle.draw()
-            tux.draw()
+					
+			if state == MAIN:
+				screen.blit(mainText, (main_xpos, main_ypos))
+				startBut.draw()
+				
+			elif state == GAME:
+				acuteBut.draw()
+				rightBut.draw()
+				obtBut.draw()
+				
+				angle.draw()
+				tux.draw()
+			
+			else:
+				screen.blit(overText, (over_xpos, over_ypos))
+				mainBut.draw()
+				restartBut.draw()
+			
             pygame.display.flip()
 
             # Try to stay at 30 FPS
